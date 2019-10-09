7eba7b25b58d9d4d954f386356df37689f445b88
Scrumpy Hat <shat@e1z2r3p26.42.us.org>
2019-10-09 14:11:12 -0700

    final changes

--- main.c
+++ main.c
@@ -6,33 +6,28 @@
 /*   By: shat <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 08:40:45 by shat              #+#    #+#             */
-/*   Updated: 2019/09/26 10:02:19 by shat             ###   ########.fr       */
+/*   Updated: 2019/10/09 13:48:31 by shat             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "get_next_line.h"
-
-int			main(int ac, char **av)
+#include <fcntl.h>
+int        main(int argc, char **argv)
 {
-	char	*file;
-	char	*line;
-	int		fd;
-	int		flag;
-
-	if (ac == 2)
-		file = av[1];
-	else
-		file = "MyFile.txt";
-	fd = open(file, O_RDONLY);
-	line = malloc(sizeof(char) * BUFF_SIZE);
-	flag = 1;
-	while (flag == 1)
-	{
-		flag = get_next_line(fd, &line);
-		printf("%s\n", line);
-	}
-	return (0);
+    int        fd;
+    char    *line;
+    if (argc == 1)
+        fd = 0;
+    else if (argc == 2)
+        fd = open(argv[1], O_RDONLY);
+    else
+        return (2);
+    while (get_next_line(fd, &line) == 1)
+    {
+        ft_putendl(line);
+        free(line);
+    }
+    if (argc == 2)
+        close(fd);
+	while(1);
 }
